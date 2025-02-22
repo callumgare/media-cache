b9d9d5e2b58229b07b3531a020e1647d5a2c4f9d
Callum Gare <callum@gare.au>
2025-02-22 22:37:16 +1100

    Order queries

--- server/api/admin/queries/index.get.ts
+++ server/api/admin/queries/index.get.ts
@@ -1,3 +1,4 @@
+import { asc } from 'drizzle-orm'
 import type { MediaFinderQuery } from 'media-finder'
 
 import { deserialize } from '~/server/lib/general'
@@ -5,7 +6,9 @@ import { deserialize } from '~/server/lib/general'
 type MediaFinderQueryOptions = ConstructorParameters<typeof MediaFinderQuery>[0]
 
 export default defineEventHandler(async () => {
-  const mediaFinderQueries = await db.query.finderQuery.findMany()
+  const mediaFinderQueries = await db.query.finderQuery.findMany({
+    orderBy: [asc(dbSchema.finderQuery.createdAt)],
+  })
   for (const mediaFinderQuery of mediaFinderQueries) {
     mediaFinderQuery.requestOptions = deserialize(mediaFinderQuery.requestOptions) as MediaFinderQueryOptions
   }
